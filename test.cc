ac30521ae58bd46b48f040ef9a2e87a8f720bb95
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2017-06-08 15:24:30 +0100

    save

--- test.cc
+++ test.cc
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+#define PI 3.14159
+
 char a; //8 bits
 char16_t b; //16 bits
 char32_t c; //32 bits
@@ -26,11 +28,14 @@ long double q; //precise double
 
 bool r;
 decltype(nullptr) t; //decltype gets the type of variable passed to it (ie typeof)
+int* ptr = nullptr;
 
-//8 bits < 256 (2^8)
-//16 buts < 65 536 (2^16)
-//32 buts < 4 294 967 296 (2^32)
-//64 buts < 18 446 744 073 709 551 616 (2^64)
+const int gravity = 9.8;
+int x = sizeof (char); // >> outputs to following
+//8 bits < 256 < (2^8) 1 byte
+//16 bits < 65 536 < (2^16) 2 bytes
+//32 bits < 4 294 967 296 < (2^32) 4 bytes
+//64 bits < 18 446 744 073 709 551 616 < (2^64) 8 bytes
 
 // \n newline
 // \r return
@@ -40,11 +45,38 @@ decltype(nullptr) t; //decltype gets the type of variable passed to it (ie typeo
 // \f form feed
 // LITERALS - \' \'' \? \\
 
+// & AND &&
+// | OR ||
+// ^ XOR
+// ~ NOT !
+// << SHL
+// >> SHR
+// new delete
+
 int main() {
-  a = 1;
-  string test = "Hello World!"; //requires #include string
+  a = (b=3,b+2);
+  int aa;
+  float bb = 3.14;
+  aa = (int) bb;
+  string test = "Hello \
+World!"; //requires #include string
   cout << test << endl;
+  cout << "enter something" << endl;
+  string _teststr;
+  getline (cin, _teststr);
+  cout << " - " << _teststr << " - " << endl;
+  int _idx = 0;
+  for (char _c : _teststr ) { //like in
+    cout << " - " << _c << " - " << endl;
+    _idx++;
+    if (_idx > 10) goto iamathing; //break; would work too while continue; skips to next iteration of loop.
+  };
+iamathing:
   return 0;
+}
+
+inline string combine(const string& a, const string& b) {
+  return a+b; //when compiled function is inserted directly rather than being looked up via a function call. Frequently used small utility functions are good for this. Most good compilers are clever enough to do this anyway so basically useless.
 }
 
 void func() {}; //not storage
